ae7ce3cbde8a5ddaccf68d676e61e5e8cf235368
sarbjotm <sarbjotm@sfu.ca>
2022-04-30 18:55:39 -0700

    Main updated to test

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from parse import *
+from words import *
 
 
 def main():
-    random_function()
+    words = []
+    create_file()
+    add_words_to_list(words)
+    print(words[0].word, words[0].POS[0], words[0].FREQ[0])
 
 
 if __name__ == "__main__":
