0053a58c0948ae5664f3bef0d1a4eb57bcd86c3b
sarbjotm <sarbjotm@sfu.ca>
2022-04-30 18:55:11 -0700

    Parsing original dataset into new dataset containing only WORD, FREQ, and POS

--- parse.py
+++ parse.py
@@ -1,6 +1,8 @@
 import pandas as pd
+from words import *
 
 
-def random_function():
-    subtlex = pd.read_excel('SUBTLEX-US frequency list with PoS information.xlsx', engine='openpyxl')
-    print(subtlex)
+def create_file():
+    subtlex = pd.read_excel('SUBTLEX-US frequency list with PoS information.xlsx')
+    new_data = subtlex.iloc[:, [0, 12, 13]]
+    new_data.to_excel('SUBTLEX-US-Compressed.xlsx', index=True, header=True)
