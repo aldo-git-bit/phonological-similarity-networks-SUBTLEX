04e83bd7920570dcd4918c1c3e38dda563b77e0f
sarbjotm <sarbjotm@sfu.ca>
2022-05-01 16:07:50 -0700

    Version 2: If we want to use FREQcount if no other information present

--- parse.py
+++ parse.py
@@ -5,5 +5,5 @@ from words import *
 
 def create_file():
     subtlex = pd.read_excel('SUBTLEX-US frequency list with PoS information.xlsx')
-    new_data = subtlex.iloc[:, [0, 12, 13]]
+    new_data = subtlex.iloc[:, [0, 1, 12, 13]]
     new_data.to_excel('SUBTLEX-US-Compressed.xlsx', index=True, header=True)
--- words.py
+++ words.py
@@ -12,7 +12,7 @@ class Words:
 # TODO: Think about what we want to do for words with N/A ratings. Currently displayed as nan but changed to N/A
 """
 For words without values in the "All_freqs_SUBTLEX" field, we could grab the number from FREQcount as well, but 
-we will be left without a Part of Speech
+we will be left without a Part of Speech, and we are unsure what the relationship between count and POS would be 
 """
 
 # TODO: Add a clause to automatically adjust the POS if needed for each word
@@ -21,6 +21,8 @@ We'll have to check with an if statement for the word if we want to automate thi
 e.g if word == "horsefly" -> words.append(word, "Noun", FREQ[0]
 
 """
+
+
 # TODO: Check if we need to change the class to append the POS to the word
 # TODO: IPA Conversion in a timely manner, and what to do if IPA conversion doesn't exist
 
@@ -32,11 +34,12 @@ def add_words_to_list(words):
         FREQ = str(df['All_freqs_SUBTLEX'][i]).split(".")
         if len(POS) == 1:
             if POS[0] == "nan" and FREQ[0] == "nan":
-                words.append(Words(word, "N/A", "N/A"))
+                # words.append(Words(word, "N/A", "N/A")) # If we want to keep it as N/A
+                words.append(Words(word, "N/A", str(df['FREQcount'][i])))  # If we want to use FREQcount
             elif POS[0] == "nan" and FREQ[0] != "nan":
                 words.append(Words(word, "N/A", FREQ[0]))
             elif POS[0] != "nan" and FREQ[0] == "nan":
-                words.append(Words(word, POS[0], "N/A"))
+                words.append(Words(word, POS[0], str(df['FREQcount'][i])))
             else:
                 words.append(Words(word, POS[0], FREQ[0]))
         else:
