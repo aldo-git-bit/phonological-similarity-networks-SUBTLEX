de5c073d7352610653996c0f7030a4cae447b971
Sarbjot Mann <sarbjotm@sfu.ca>
2022-06-13 21:36:13 -0700

    Edited bash script to produce .txt file

--- ipa_translator.sh
+++ ipa_translator.sh
@@ -15,13 +15,10 @@ fi
 echo "The word you chose for translation: $current"
 
 #format the word into '< w o r d >'
-onmt_word=$(python ONMT_DATA_FORMAT.py "$current")
+onmt_word=$(python3.9 ONMT_DATA_FORMAT.py "$current")
 #write over current word in temp_word.txt
 echo $onmt_word > temp_word.txt
 
 #translate the word in temp_word.txt using openNMT with the model created from a set of 32k words
-results=$(onmt_translate -gpu 0 -model eng_ipa_model1_steps/eng_ipa_model1_step_100000.pt -src temp_word.txt -replace_unk -verbose -output model1_step_10000_temp_pred)
+results=$(onmt_translate -gpu -1 -model eng_ipa_model1_steps/eng_ipa_model1_step_100000.pt -src temp_word.txt -replace_unk -verbose -output ipa_translation.txt)
 #print only the results we care about, i.e. the translated IPA
-echo "The provided IPA translation is:"
-echo $results | egrep -o '< .* >'
-echo "Please ignore the first and last symbol, these are needed for the translation process."
