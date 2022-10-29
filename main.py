404df6ee87a26922a29cd7135171337be7761ec4
Sarbjot Mann <sarbjotmann98@gmail.com>
2022-10-29 12:50:35 -0700

    Added arguments and checks

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from parse import *
 from words import *
 from graph import *
 from smallworlds import *
+import os
 import globals
 import argparse
 import eng_to_ipa as ipa
@@ -40,35 +41,99 @@ import concurrent.futures
     # update_file(subtlex_dataset)
     # create_adjanceylist(subtlex_dataset)
     # create_graph()
+
+def subtlex_file_error(error):
+    print("Error: Expected a value of True or False. Got {error}")
+
+def lemma_wordforms_error(error):
+    print("Error: Expected a value of 'lemma' or 'wordforms'. Got {error}")
+
+def lemma_size_error(error):
+    print("Error: Range specified is out of bounds. Range from 1 - 51228 inclusive was expected, got {error}")
+
+def word_size_error(error):
+    print("Error: Range specified is out of bounds. Range from 1 - 74287 inclusive was expected, got {error}")
+
+def edge_list_file_error(error):
+    print("Error: File Not Found. Make sure the pathing is correct")
         
 
 if __name__ == "__main__":  
     parser = argparse.ArgumentParser(description = 'Parametres')
 
-    parser.add_argument('-sf','--subtlexfile', type = bool, metavar = '', help = 'Do you have the IPA Subtlex File (True/False)', required=True)
-    parser.add_argument('-al', '--edgelist', type = bool, metavar = '', help = 'Already Generated Adjancey/Edge List? (True/False)')
+    parser.add_argument('-sf','--subtlexfile', type = str, metavar = '', help = 'Do you have the IPA Subtlex File (True/False)', required=True)
+    parser.add_argument('-al', '--edgelist', type = str, metavar = '', help = 'Name of adjancey list file if you already have one')
+    parser.add_argument('-lw', '--lemmawords', type = str, metavar = '', help = 'Specify whether you wanted to use "lemma" or "wordforms" ', required=True)
     parser.add_argument('-size', '--size', type = int, metavar = '', help = "Size of Adjancey List", required=True)
-    parser.add_argument('-r', '--random', type = bool, metavar = '', help = "Random Sample (True/False)")
+    # parser.add_argument('-r', '--random', type = str, metavar = '', help = "Random Sample (True/False)")
 
     args = parser.parse_args()
 
 
-    # globals.initialize()
     
-    subtlex_dataset = []
-
-    if args.subtlexfile:
-        add_words_to_list_from_file(subtlex_dataset)
+    # Check conditions 
+    if args.subtlexfile.lower().strip() not in ['f', 'false', 't', 'true']:
+        subtlex_file_error(args.subtlexfile)
+    
+    elif args.lemmawords.lower().strip() not in ['lemma', 'wordforms']:
+        lemma_wordforms_error(args.lemmawords)
+    
+    elif args.lemmawords.lower().strip() == 'lemma' and (args.size <= 0 or args.size >= 51229 ):
+        lemma_size_error(args.size)
+    
+    elif args.lemmawords.lower().strip() == 'wordforms' and (args.size <= 0 or args.size >= 74288 ):
+        word_size_error(args.size)
+    
+    elif args.edgelist is not None and os.path.isfile(args.edgelist):
+        edge_list_file_error(args.edgelist)
+    
+    #Run program here based on arguments
     else:
-        add_words_to_list(subtlex_dataset)
+
+        subtlex_dataset = []
+        lemmas = []
+        if args.subtlex
+
+        if args.edgelist is not None and os.path.isfile(args.edgelist):
+            create_graph(args.edgelist)
+        
+        elif args.subtlexfile.lower().strip() in ['t', 'true']:
+            add_words_to_list_from_file(subtlex_dataset)
+        else:
+            create_file()
+            add_words_to_list(subtlex_dataset)
+            update_file()
+
+        if args.subtlex.lemmawords.lower().strip() =='lemma':
+            number_of_lemmas(subtlex_dataset, lemmas)
+            update_list(args.size, lemmas)
+            update_ipa(lemmas)
+            filename = create_adjanceylist_lemma(args.size, lemmas)
+            create_graph(filename)
+        
+        else:
+            update_list(args.size, subtlex_dataset)
+            update_ipa(subtlex_dataset)
+            filename = create_adjanceylist(args.size, subtlex_dataset)
+            create_graph(filename)
+
+
     
-    print(subtlex_dataset[0].WORD)
-    update_list(args.size, subtlex_dataset)
+
     
-    update_ipa(subtlex_dataset)
+
+    # if args.subtlexfile:
+    #     add_words_to_list_from_file(subtlex_dataset)
+    # else:
+    #     add_words_to_list(subtlex_dataset)
+    
+    # print(subtlex_dataset[0].WORD)
+    # update_list(args.size, subtlex_dataset)
+    
+    # update_ipa(subtlex_dataset)
 
 
     # # print_global()
     
-    create_adjanceylist(subtlex_dataset)
+    # create_adjanceylist(subtlex_dataset)
     # create_graph()
