074e7833bedf3795253b36174a64bca6fe2ff86e
Sarbjot Mann <sarbjotmann98@gmail.com>
2023-05-07 12:09:16 -0700

    Removed unused code in comments

--- main.py
+++ main.py
@@ -11,37 +11,6 @@ import jellyfish
 import concurrent.futures
 
 
-# def main():
-    # similar = 0
-    # sed_ignoring_zerocase = 0
-    # sed = 0
-    # no_ipa = 0
-    # subtlex_dataset
-    # subtlex_dataset = []
-    # lemmas = []
-    # random_sample()
-    # print("1")
-    # create_file() 
-    # print("2")
-    # add_words_to_list_from_file(subtlex_dataset)
-    # number_of_lemmas(subtlex_dataset, lemmas)
-    # print_global()
-    # print(len(lemmas))
-
-    # for i in range(0, 500):
-    #     print(lemmas[i])
-    # update_ipa(subtlex_dataset)
-    # print(len(subtlex_dataset))
-    # # nan = words_without_pos(subtlex_dataset)
-    # # freq = total_pos(subtlex_dataset, nan)
-    # # print(freq)
-    # # total_pos_reading_from_file()
-    # # frequency_distribution(freq)
-    # print("3")
-    # update_file(subtlex_dataset)
-    # create_adjanceylist(subtlex_dataset)
-    # create_graph()
-
 def subtlex_file_error(error):
     print("Error: Expected a value of True or False. Got {error}")
 
@@ -54,19 +23,13 @@ def lemma_size_error(error):
 def word_size_error(error):
     print("Error: Range specified is out of bounds. Range from 1 - 74287 inclusive was expected, got {error}")
 
-def edge_list_file_error(error):
-    print("Error: File Not Found. Make sure the pathing is correct, and the file is an .adjlist")
-        
 
 if __name__ == "__main__":  
     parser = argparse.ArgumentParser(description = 'Parametres')
 
     parser.add_argument('-sf','--subtlexfile', type = str, metavar = '', help = 'Do you have the IPA Subtlex File (True/False)', required=True)
-    parser.add_argument('-al', '--edgelist', type = str, metavar = '', help = 'Name of adjancey list file if you already have one')
     parser.add_argument('-lw', '--lemmawords', type = str, metavar = '', help = 'Specify whether you wanted to use "lemma" or "wordforms" ', required=True)
     parser.add_argument('-size', '--size', type = int, metavar = '', help = "Size of Adjancey List", required=True)
-    # parser.add_argument('-r', '--random', type = str, metavar = '', help = "Random Sample (True/False)")
-
     args = parser.parse_args()
     
     
@@ -83,8 +46,6 @@ if __name__ == "__main__":
     elif args.lemmawords.lower().strip() == 'wordforms' and (args.size <= 0 or args.size >= 74288 ):
         word_size_error(args.size)
     
-    elif args.edgelist is not None and ( os.path.isfile(args.edgelist) and os.path.splitext(file_path)[1].lower() == ".adjlist" ):
-        edge_list_file_error(args.edgelist)
     
     #Run program here based on arguments
     else:
@@ -109,31 +70,11 @@ if __name__ == "__main__":
             update_list(args.size, lemmas)
             update_ipa(lemmas)
             filename = create_adjanceylist_lemma(args.size, lemmas)
-            # create_graph(filename)
+            
         
         else:
             update_list(args.size, subtlex_dataset)
             update_ipa(subtlex_dataset)
             filename = create_adjanceylist(args.size, subtlex_dataset)
-            # create_graph(filename)
+            
 
-
-    
-
-    
-
-    # if args.subtlexfile:
-    #     add_words_to_list_from_file(subtlex_dataset)
-    # else:
-    #     add_words_to_list(subtlex_dataset)
-    
-    # print(subtlex_dataset[0].WORD)
-    # update_list(args.size, subtlex_dataset)
-    
-    # update_ipa(subtlex_dataset)
-
-
-    # # print_global()
-    
-    # create_adjanceylist(subtlex_dataset)
-    # create_graph()
