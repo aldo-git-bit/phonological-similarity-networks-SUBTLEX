162aba9cee6edd01537b7a84cc9f090be118bbef
Sarbjot Mann <44513277+sarbjotm@users.noreply.github.com>
2024-10-06 23:21:45 -0700

    Adding new files to do a new analysis

---
+++ word-length-freq.py
@@ -0,0 +1,86 @@
+import eng_to_ipa as ipa
+import pandas as pd 
+
+class Words:
+    def __init__(self, WORD, IPA, IPA_LIST, FREQ):
+        self.WORD = WORD
+        self.IPA = IPA
+        self.IPA_LIST = IPA_LIST
+        self.FREQ = FREQ
+        self.LENGTH = ""
+
+
+def add_words_to_list(words):
+    df = pd.read_excel('words_giant.xlsx')
+    for i in range(len(df['word'])):
+        WORD = str(df['word'][i]).strip()
+        IPA = str(ipa.convert(WORD)).strip()
+        IPA = IPA.replace("ˈ", "")
+        IPA = IPA.replace("ˌ", "")
+        if IPA[len(IPA) - 1] == "*":
+            IPA = str(subprocess.run(['bash', 'ipa_translator.sh', WORD]))
+            with open('ipa_translation.txt') as f:
+                lines = f.readlines()
+                IPA = lines[0][1:].replace(" ", "")
+                IPA = IPA.replace(">", " ")
+                IPA = IPA.strip()
+                IPA = IPA.strip("\n")
+            
+        FREQcount = int(df['frequency'][i])
+        words.append(Words(WORD, IPA, list(IPA), FREQcount))
+
+def update_ipa(words):
+    two_character_phonemes = ["oʊ", "ɔɪ", "aɪ", "aʊ"]
+    vowels = ["ɑ", "æ", "ə", "ʌ", "ɔ", "a", "aɪ", "aʊ", "ɛ", "e", "ɪ", "i", "o", "ɔ", "ʊ", "u"]
+
+    for i in range(0, len(words)):
+        word_1 = words[i].IPA_LIST
+        temp_array_word = []
+        temp_array_word.append(word_1[0])
+
+        for k in range(1, len(word_1)):
+            temp_array_word.append(word_1[k])
+
+            if (word_1[k-1] + word_1[k] == "ər") and (k != len(word_1) - 1) and (word_1[k+1] not in vowels):
+                    # print(f"{words[i].WORD}...... {word_1}.....{word_1[k+1]}")
+                    temp_array_word.pop()
+                    temp_array_word.pop()
+                    temp_array_word.append(word_1[k-1] + word_1[k])
+
+
+            elif (word_1[k-1] + word_1[k] in two_character_phonemes):
+                temp_array_word.pop()
+                temp_array_word.pop()
+                temp_array_word.append(word_1[k-1] + word_1[k])
+        
+        words[i].IPA_LIST = temp_array_word
+
+
+
+def get_length(words):
+    for i in range(0, len(words)):
+        words[i].LENGTH = len(words[i].IPA_LIST)
+
+
+def update_excel_with_ipa(words):
+    df = pd.read_excel('words_giant.xlsx')
+    ipa_column = [word.IPA for word in words]
+    ipa_list_column = [','.join(word.IPA_LIST) for word in words] 
+    length_column = [word.LENGTH for word in words]
+
+    df['IPA'] = ipa_column
+    df['IPA_LIST'] = ipa_list_column
+    df['Word_Length'] = length_column
+
+    df.to_excel('words_giant_updated.xlsx', index=False)  
+
+
+new_list = []
+add_words_to_list(new_list)
+update_ipa(new_list)
+get_length(new_list)
+update_excel_with_ipa(new_list)
+
+
+
+
